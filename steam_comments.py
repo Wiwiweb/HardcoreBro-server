9f2e21a3845041090ba9d42ea88c1d71984d360c
Wiwiweb <wiwiweb2@gmail.com>
2014-09-22 14:50:33 -0700

    Add get_steam_comments function

--- steam_comments.py
+++ steam_comments.py
@@ -1,15 +1,39 @@
+import collections
+import re
+import time
+
 from bs4 import BeautifulSoup
 import pymysql
 import requests
 
 import config
 
-STEAM_PAGE = "http://steamcommunity.com/groups/vohcb"
+
+STEAM_PAGE = 'http://steamcommunity.com/groups/vohcb'
+EMOTICON_REGEX = r'<img class="emoticon" src="https?://.+\..{3}/economy/emoticon/(\w+)"/>'
+
+SteamComment = collections.namedtuple('SteamComment', 'id author avatar text date')
 
 
 def get_steam_comments():
     req = requests.get(STEAM_PAGE)
-    print(req.text)
+    soup = BeautifulSoup(req.text)
+    all_comments = soup.find_all('div', class_='commentthread_comment')
+    comment_list = []
+    for comment in all_comments:
+        comment_id = comment['id'].split('_')[1]
+        author = comment.find('div', class_='commentthread_comment_author').a.string.strip()
+        avatar = comment.find('div', class_='commentthread_comment_avatar').img['src']
+
+        date = comment.find('span', class_='commentthread_comment_timestamp').string.strip()
+        date = time.strptime(date, '%d %b @ %I:%M%p')
+
+        text = comment.find('div', class_='commentthread_comment_text').renderContents()
+        text = text.decode("utf-8").strip()
+        text = re.sub(EMOTICON_REGEX, r':\1:', text)
+
+        comment_list.append(SteamComment(comment_id, author, avatar, text, date))
+    return comment_list
 
 
 def update_database(steam_comments):
@@ -33,4 +57,6 @@ def update_database(steam_comments):
 
 if __name__ == '__main__':
     comments = get_steam_comments()
-    update_database(comments)
+    for c in comments:
+        print(c)
+    # update_database(comments)
